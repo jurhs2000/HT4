4345481431d110a2dd6b81a0f518c39b23705272
Julio Herrera <jurhs2000@gmail.com>
2020-02-26 19:33:10 -0600

    decision de stack lista

--- calculator/ListFactory.java
+++ calculator/ListFactory.java
@@ -1,3 +1,21 @@
-class ListFactory {
-    
+class ListFactory<E> {
+    //se utiliza el patron Factory
+  
+  /**
+   * Selecciona la implementacion a utilizar para un stack
+   * @param listType tipo de lista a utilizar: enlazada, doble enlazada o circular
+   * @return una pila de cualquier tipo
+   */
+  public Stack<E> getStack(int listType) {
+    switch (listType) {
+      case 1:
+        return new SinglyLinkedList<E>();
+      case 2:
+        return new DoublyLinkedList<E>();
+      case 3:
+        return new CircularList<E>();
+      default:
+        return null;
+    }
+  }
 }
