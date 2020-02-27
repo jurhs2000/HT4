91a3803bf924bc417da557d035982942526bfdb8
Julio Herrera <jurhs2000@gmail.com>
2020-02-26 19:30:41 -0600

    decision de stack

---
+++ calculator/ListFactory.java
@@ -0,0 +1,3 @@
+class ListFactory {
+    
+}
--- calculator/StackFactory.java
+++ calculator/StackFactory.java
@@ -8,10 +8,16 @@ class StackFactory<E> {
    * @return una pila de cualquier tipo
    */
   public Stack<E> getStack(int stackType, int listType) {
-  // seleccion de la implementacion a utilizar:
-  /*if (entry.equals("AL"))
-    return new StackArrayList<E>(); //regresa ArrayList
-  else*/
-    return new StackVector<E>(); //regresa Vector
+    switch (stackType) {
+      case 1:
+        return new StackArrayList<E>();
+      case 2:
+        return new StackVector<E>();
+      case 3:
+        ListFactory listFactory = new ListFactory();
+        return listFactory.getStack(listType);
+      default:
+        return null;
+    }
   }
 }
